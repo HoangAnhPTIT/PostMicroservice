1af967223152833ab9645280bac257dcf075008d
HoangAnh <caohoanganh2000@gmail.com>
2023-02-17 17:43:42 +0700

    init

---
+++ CQRS.Core/CQRS.Core/Events/BaseEvent.cs
@@ -0,0 +1,11 @@
+﻿using CQRS.Core.Messages;
+
+namespace CQRS.Core.Events
+{
+    public abstract class BaseEvent : Message
+    {
+        public int Version { get; set; }    
+
+        public string Type { get; set; }
+    }
+}
---
+++ SM-Post/Post.Common/Events/PostRemovedEvent.cs
@@ -0,0 +1,11 @@
+﻿using CQRS.Core.Events;
+
+namespace Post.Common.Events
+{
+    internal class PostRemovedEvent : BaseEvent
+    {
+        public PostRemovedEvent() : base(nameof(PostRemovedEvent))
+        {
+        }
+    }
+}
